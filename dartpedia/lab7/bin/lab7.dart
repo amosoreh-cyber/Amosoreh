91708282090775253a06bbfd67aa2740f25caef6
Vladimirov Yaroslav Igorevich <amosoreh@gmail.com>
2026-04-09 12:11:11 +0300

    Поддержка цветного вывода

---
+++ dartpedia/lab7/bin/lab7.dart
@@ -0,0 +1,5 @@
+import 'package:lab7/lab7.dart' as lab7;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lab7.calculate()}!');
+}
---
+++ dartpedia/lab7/test/lab7_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab7/lab7.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
