d2cd040cd0741e1453d48d854fb7292b53eeab52
Vladimirov Yaroslav Igorevich <amosoreh@gmail.com>
2026-04-09 12:35:53 +0300

    Тестирование

---
+++ dartpedia/lr10/wikipedia/lib/src/model/article.dart
@@ -0,0 +1,35 @@
+/// Класс для представления статьи
+class Article {
+  Article({required this.title, required this.extract});
+
+  final String title;
+  final String extract;
+
+  /// Создает список статей из JSON-ответа
+  static List<Article> listFromJson(Map<String, Object?> json) {
+    final List<Article> articles = <Article>[];
+    if (json case {'query': {'pages': final Map<String, Object?> pages}}) {
+      for (final MapEntry<String, Object?>(:Object? value) in pages.entries) {
+        if (value case {
+          'title': final String title,
+          'extract': final String extract,
+        }) {
+          articles.add(Article(title: title, extract: extract));
+        }
+      }
+      return articles;
+    }
+    throw FormatException('Could not deserialize Article, json=$json');
+  }
+
+  /// Преобразует статью в JSON-объект
+  Map<String, Object?> toJson() => <String, Object?>{
+    'title': title,
+    'extract': extract,
+  };
+
+  @override
+  String toString() {
+    return 'Article{title: $title, extract: $extract}';
+  }
+}
