78de8484f9cf7af20cc5e41483b447a5de49e6a6
Vladimirov Yaroslav Igorevich <amosoreh@gmail.com>
2026-04-09 05:03:14 +0300

    Протестировал версию

---
+++ dartpedia/lab2/lib/lab2.dart
@@ -0,0 +1,9 @@
+const version = '3.11.3'; 
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Hello, Dart!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  }
+}
