a4276ab1532b05096d0fecfce533bf7854ecb532
Vladimirov Yaroslav Igorevich <amosoreh@gmail.com>
2026-04-09 11:50:26 +0300

    Определил связь между классами

---
+++ dartpedia/lab5/command_runner/lib/src/command_runner_base.dart
@@ -0,0 +1,36 @@
+import 'dart:collection';
+import 'dart:io';
+import 'arguments.dart';
+
+class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  Future<void> run(List<String> input) async {
+    final ArgResults results = parse(input);
+    if (results.command != null) {
+      Object? output = await results.command!.run(results);
+      if (output != null) print(output.toString());
+    }
+  }
+
+  void addCommand(Command command) {
+    _commands[command.name] = command;
+    command.runner = this;
+  }
+
+  ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    if (input.isNotEmpty && _commands.containsKey(input.first)) {
+      results.command = _commands[input.first];
+    }
+    return results;
+  }
+
+  String get usage {
+    final exeFile = Platform.script.path.split('/').last;
+    return 'Usage: dart bin/$exeFile <command> [commandArg?] [...options?]';
+  }
+}
